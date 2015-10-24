5ca3e2a4c212f5324ebccd3e56cba06b6b9fdc3f
Andrew Yang <andrewyang96@gmail.com>
2015-10-24 01:28:48 -0500

    Random word path added

--- httpd.go
+++ httpd.go
@@ -3,11 +3,44 @@ package main
 import (
   "os"
   "github.com/gin-gonic/gin"
+  "bufio"
+  "math/rand"
+  "time"
 )
+
+// Set numWords constant
+const numWords int = 5000
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func randChoice(arr [numWords]string) (string) {
+  rand.Seed(time.Now().Unix())
+  idx := rand.Intn(len(arr))
+  return arr[idx]
+}
 
 func main() {
   // Create gin router
   r := gin.Default()
+
+  // Create word array
+  var words [numWords]string
+
+  // Read from 5000-common-words.txt
+  f, err := os.Open("5000-common-words.txt")
+  check(err)
+  scanner := bufio.NewScanner(f)
+  scanner.Split(bufio.ScanLines)
+
+  // Fetch words
+  for i := 0; i < numWords; i++ {
+    scanner.Scan()
+    words[i] = scanner.Text()
+  }
 
   /* Begin routes */
 
@@ -16,9 +49,10 @@ func main() {
     c.String(200, "index")
   })
 
-  // Ping
-  r.GET("/ping", func(c *gin.Context) {
-    c.String(200, "pong")
+  // Random word
+  r.GET("/randword", func(c *gin.Context) {
+    myWord := randChoice(words)
+    c.String(200, myWord)
   })
 
   /* End routes */
