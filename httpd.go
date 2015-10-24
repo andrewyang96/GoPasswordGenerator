c748a7690d0d17188df3b8d3d86ad39f28728503
Andrew Yang <andrewyang96@gmail.com>
2015-10-24 01:08:12 -0500

    Reformat httpd.go

--- httpd.go
+++ httpd.go
@@ -6,15 +6,22 @@ import (
 )
 
 func main() {
+  // Create gin router
   r := gin.Default()
 
-  // Routes
+  /* Begin routes */
+
+  // Index
   r.GET("/", func(c *gin.Context) {
     c.String(200, "index")
   })
+
+  // Ping
   r.GET("/ping", func(c *gin.Context) {
     c.String(200, "pong")
   })
+
+  /* End routes */
 
   // Run server
   port := os.Getenv("PORT")
