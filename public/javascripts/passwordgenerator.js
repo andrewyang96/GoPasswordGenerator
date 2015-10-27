f98517c05e762574d2d0b99f2b6d8f426728e67e
Andrew Yang <andrewyang96@gmail.com>
2015-10-26 20:08:27 -0500

    Got one select option to work in react

--- public/javascripts/passwordgenerator.js
+++ public/javascripts/passwordgenerator.js
@@ -1,42 +1,76 @@
-var PasswordOptionSelector = React.createClass({
+var PasswordOptionSelect = React.createClass({
 	getDefaultProps: function () {
-		return {key: "defaultkey", title: "Title not assigned", options: []};
+		return {optkey: "notset", optval: {title: "Not Set"}}
 	},
 
 	render: function () {
-		var createOption = function (opt) {
-			return <option key={opt.key} value={opt.key}>{opt.title}</option>;
-		};
+		return <option key={this.props.optkey} value={this.props.optkey}>{this.props.optval.title}</option>;
+	}
+});
+
+var PasswordOptionSelector = React.createClass({
+	getDefaultProps: function () {
+		return {name: "notAssigned", title: "Default Title", options: {}};
+	},
+
+	getInitialState: function () {
+		return {value: "null"};
+	},
+
+	render: function () {
+		var createOption = function (optkey) {
+			var optval = this.props.options[optkey];
+			return <PasswordOptionSelect optkey={optkey} optval={optval} />
+		}.bind(this);
 		return (
 		<div>
 			<label htmlFor={this.props.name}>{this.props.title}</label>
-			<select name={this.props.name} onChange={this.props.onChange}>
-				{this.props.options.map(createOption)}
+			<select name={this.props.name} value={this.props.value} onChange={this.props.onChange}>
+				<PasswordOptionSelect optkey="null" optval={{title: "<Nothing>"}} />
+				<PasswordOptionSelect optkey="random" optval={{title: "<Random>"}} />
+				{Object.keys(this.props.options).map(createOption)}
 			</select>
 		</div>);
 	}
 });
 
 var PasswordGenerator = React.createClass({
-	getDefaultProps: function () {
-		return {}; // TODO
+	getInitialState: function () {
+		return {
+			delimiterValue: "null"
+		};
+	},
+
+	componentDidMount: function () {
+		$.getJSON("/characters", function (data) {
+			// console.log(data);
+			this.setState(data);
+		}.bind(this));
 	},
 
 	getInitialState: function () {
 		return {words: []};
 	},
 
+	handleDelimiterChange: function (event) {
+		console.log(event);
+		this.setState({delimiterValue: event.target.value});
+	},
+
 	render: function () {
-		var testArr = [{char:'a', name:'Ay'}, {char:'b', name:'Bee'}, {char:'c', name:'See'}];
+		var testObj = {
+			"ayy": {title: "A"},
+			"bee": {title: "B"},
+			"see": {title: "C"}
+		};
 		return (
 		<div class="text-center">
 			<h2>Password Generator</h2>
-			<PasswordOptionSelector name="test" title="Test" options={testArr} />
+			<PasswordOptionSelector name="test" title="Test" options={testObj} value={this.state.delimiterValue} onChange={this.handleDelimiterChange} />
 		</div>);
 	}
 });
 
 $(document).ready(function () {
-	console.log("Here!");
 	React.render(<PasswordGenerator />, $("#passwordgenerator").get(0));
 });
